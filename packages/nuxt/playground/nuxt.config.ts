18667b20dbbad7dafc4a1a5fe4bf91732e693927
adamcikado <adamcikado@gmail.com>
2023-03-03 13:20:56 +0100

    docs: fix build

--- packages/nuxt/playground/nuxt.config.ts
+++ packages/nuxt/playground/nuxt.config.ts
@@ -4,7 +4,7 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   alias: {
     '@outloud/vue-modals/style.css': fileURLToPath(new URL('../../vue-modals/dist/style.css', import.meta.url)),
-    '@outloud/vue-modals': fileURLToPath(new URL('../../vue-modals/dist/index.es.js', import.meta.url)),
+    '@outloud/vue-modals': fileURLToPath(new URL('../../vue-modals/dist/index.mjs', import.meta.url)),
   },
   modules: ['../src/module'],
 })
